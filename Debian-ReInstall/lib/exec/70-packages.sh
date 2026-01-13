c9960434454abdca7b69829ae7c48ca197bd8d0f
Aleksandr <fox4ua@ya.ru>
2026-01-13 13:53:47 +0200

    fix

--- Debian-ReInstall/lib/exec/70-packages.sh
+++ Debian-ReInstall/lib/exec/70-packages.sh
@@ -30,10 +30,12 @@ prepare_install_resolv_conf() {
 ensure_chroot_dns() {
   if [[ -f "$TARGET_DIR/etc/resolv.conf" ]]; then
     ensure_target_nsswitch_dns
-    if chroot_run_quiet "getent hosts deb.debian.org >/dev/null"; then
+    if chroot_run_quiet "getent hosts deb.debian.org >/dev/null" \
+      && chroot_run_quiet "getent hosts security.debian.org >/dev/null"; then
       return 0
     fi
   fi
+
 
   prepare_install_resolv_conf
 
