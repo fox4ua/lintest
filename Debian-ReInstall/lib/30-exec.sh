fb838de8889fb5757dbbfb451491154275c4a6d6
Aleksandr <fox4ua@users.noreply.github.com>
2026-01-13 14:37:23 +0200

    Move disk release logic into exec module

--- Debian-ReInstall/lib/30-exec.sh
+++ Debian-ReInstall/lib/30-exec.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
 : "${EXEC_DIR:?}"
+
+source "$EXEC_DIR/10-release.sh"
+
+execute_install() {
+  stage "execute"
+  log "[=] execution started"
+
+  if (( DISK_NEEDS_RELEASE )); then
+    if (( DISK_RELEASE_APPROVED )); then
+      exec_release_disk_resources "$DISK"
+    else
+      log "[!] disk requires release but user did not approve"
+    fi
+  fi
+
+  ui_msg "Execution steps are not implemented yet."
+}
